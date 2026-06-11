4fabf0f5447ea2a5bddca216a9d746e46434e242
Lorenz Hubel <lh9809@della-mol.princeton.edu>
2026-06-11 18:21:08 -0400

    Initial commit: Small Python package structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Deconvolving Kernel Package
+
+The aim of this package is to share the code
+used in this Master thesis to others.
---
+++ src/deconvolving_kernel/__init__.py
@@ -0,0 +1,6 @@
+"""Deconvolving kernel regression.
+
+Code accompanying the Master thesis on deconvolving kernel regression.
+"""
+
+__version__ = "0.1.0"
